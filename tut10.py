49d1b0550926934103d632f18e59e94d8288e92b
hawkeyepirate <lordhawkeye25@gmail.com>
2021-04-26 15:51:28 +0530

    Third

--- tut10.py
+++ tut10.py
@@ -1,4 +1,4 @@
-list1 = ["akshat", 56, "riya", 1, "Sandeep", 33, 6, 12, 2, "Nidhi", "Asha", "OP", 15]
+list1 = ["akshat", 56, "riya", 1, "Sandeep", 33, 6, 12, 2, "Nidhi", "Asha", "OP", 15, int, float]
 for item in list1:
     if type(item) == int:
         if item > 6:
