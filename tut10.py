4b23c0d0c9bfaae5c94971dcce9f7a3a747e6f86
hawkeyepirate <lordhawkeye25@gmail.com>
2021-04-26 11:44:58 +0530

    Second

---
+++ tut10.py
@@ -0,0 +1,6 @@
+list1 = ["akshat", 56, "riya", 1, "Sandeep", 33, 6, 12, 2, "Nidhi", "Asha", "OP", 15]
+for item in list1:
+    if type(item) == int:
+        if item > 6:
+            print(item)
+       
--- tut9.py
+++ tut9.py
@@ -1,22 +1,40 @@
                     # Faulty Calculaator
 
-print("KINDLY ENTER THE OPERATOR FROM  +, -, *, /")
-operator = int(input)
-print("Enter The First Number")
-firstNumber = int(input)
-print("Enter Second Number")
-secondNumber = int(input)
-
-answer = firstNumber operator secondNumber
-
-if answer == 45 * 3:
-    answer =555
-    print(answer)
-elif answer == 56 + 9:
-    answer = 77
-    print(answer)
-elif answer == 56 / 6:
-    answer = 4
-    print(answer)
+# print("KINDLY ENTER THE OPERATOR FROM  +, -, *, /")
+print("Enter 1 for add, 2 for substract, 3 for multiply, 4 for divide")
+x = int(input())
+if x == 1:
+    print("Enter The value of First Number")
+    y = int(input())
+    print("Enter The Second Value")
+    z = int(input())
+    if y==56 and z == 9:
+        print("Answer is 77")
+    else:
+        print("Sum  is ",(y+z))
+elif x == 2:
+    print("Enter The value of First Number")
+    y = int(input())
+    print("Enter The Second Value")
+    z = int(input())
+    print("The difference is",(y-z))
+elif x == 3:
+    print("Enter The value of First Number")
+    y = int(input())
+    print("Enter The Second Value")
+    z = int(input())
+    if y == 45 and z == 3:
+        print("Answer is 555")
+    else:
+        print("Product is",(y*z))
+elif x == 4:
+    print("Enter The value of First Number")
+    y = int(input())
+    print("Enter The Second Value")
+    z = int(input())
+    if y == 56 and z == 6:
+        print("Answer is 4")
+    else:
+        print("Quotient is",(y/z))
 else:
-    print(answer)
+    print("You have not entered a valid Operator. Please Check")
