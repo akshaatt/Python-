4dd7b5081357cd8d5f1d80591fd7b2008875c38b
hawkeyepirate <lordhawkeye25@gmail.com>
2021-04-29 21:19:50 +0530

    d

---
+++ tut16.py
@@ -0,0 +1,16 @@
+# OPERATORS N PYTHON
+
+# arthematic operators
+print(5+6)
+print(5-6)
+print(5*6)
+print(5/6)
+print(5//6)
+print(5**6)
+if 6 > 5:
+    print("hide")
+else:
+    print("SEEn")
+limst = [1,2,3,4,5,6,7,89,8,9]
+limst.remove(8)
+print(limst)
