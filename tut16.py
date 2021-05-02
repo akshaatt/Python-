2ce52b9ce4788c7fea49e36bcfc123dcc789889d
hawkeyepirate <lordhawkeye25@gmail.com>
2021-05-02 23:14:17 +0530

    xyz

--- tut16.py
+++ tut16.py
@@ -10,10 +10,8 @@ print(5**6)
 if 6 > 5:
     print("hide")
 else:
-    print("SEEn")
+    print("Seen")
 limst = [1,2,3,4,5,6,7,89,8,9]
 limst.remove(8)
 print(limst)
-#sorry no code today
-#sorry no code today
-#sorry no code today
+
---
+++ tut17.py
@@ -0,0 +1,13 @@
+# a = 56
+# b = 23
+# if (a > b ): print(a+b)
+    
+# int =5
+# while int <=-1:
+#     print (int)
+#     int = int -1
+
+int =3
+while int >=2:
+    print (int)
+    int = 1
